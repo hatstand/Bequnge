3cb4cdb3d09fc841b43f695d9c725f076607705e
David Sansome <me@davidsansome.com>
2008-05-06 03:48:01 +0000

    Fixed shader error

--- src/shaders/char_frag.glsl
+++ src/shaders/char_frag.glsl
@@ -2,7 +2,7 @@ uniform sampler2D tex;
 
 void main()
 {
-	vec4 alpha = texture2D(tex, gl_TexCoord[0].xy).a;
+	float alpha = texture2D(tex, gl_TexCoord[0].xy).a;
 	
 	vec3 color = gl_Color.rgb * alpha;
 	
